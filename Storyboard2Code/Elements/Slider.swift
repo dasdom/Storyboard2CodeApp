b9d66fde90422f60f3402438dc2d112ffe9409fb
Dominik Hauser <dom@dasdom.de>
2016-05-09 22:04:16 +0200

    Slider implementation

--- Storyboard2Code/Elements/Slider.swift
+++ Storyboard2Code/Elements/Slider.swift
@@ -9,25 +9,31 @@
 import Foundation
 
 public class Slider: View {
-  let minValue: Float
-  let maxValue: Float
+  let minimumValue: Float
+  let maximumValue: Float
   let value: Float
+  let continuous: Bool?
   
   public required init(dict: [String : String]) {
-    minValue = Float(dict["minValue"]!)!
-    maxValue = Float(dict["maxValue"]!)!
+    minimumValue = Float(dict["minValue"]!)!
+    maximumValue = Float(dict["maxValue"]!)!
     value = Float(dict["value"]!)!
+    continuous = dict["continuous"].flatMap { $0 == "YES" }
     
     super.init(dict: dict)
     
     type = ElementType.UISlider
   }
   
-  public override var setupString: String {
-    var string = super.setupString
-    string += "\(userLabel).minimumValue = \(minValue)\n"
-    string += "\(userLabel).maximumValue = \(maxValue)\n"
-    string += "\(userLabel).value = \(value)\n"
-    return string
+  override func reflectable() -> [String] {
+    var temp = super.reflectable()
+    temp.append("minimumValue")
+    temp.append("maximumValue")
+    temp.append("value")
+    temp.append("continuous")
+    
+    /// From the docs: "The opaque property has no effect in system-provided classes such as UIButton, UILabel, UITableViewCell, and so on."
+    temp = temp.filter { $0 != "opaque" }
+    return temp
   }
 }
