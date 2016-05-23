117d88e7a10b62024a1ba127834445a326076704
Dominik Hauser <dom@dasdom.de>
2016-05-23 08:50:47 +0200

    Refactoring

--- Elements.playground/Contents.swift
+++ Elements.playground/Contents.swift
@@ -2,23 +2,4 @@
 
 import UIKit
 
-let defaultScrollView = UIScrollView()
-defaultScrollView.clipsToBounds == true
-defaultScrollView.multipleTouchEnabled == true
-defaultScrollView.contentMode == .ScaleToFill
-
-let fooScrollView = UIScrollView()
-fooScrollView.directionalLockEnabled = true
-fooScrollView.bounces = false
-fooScrollView.alwaysBounceVertical = true
-fooScrollView.scrollEnabled = false
-fooScrollView.pagingEnabled = true
-fooScrollView.showsHorizontalScrollIndicator = false
-fooScrollView.showsVerticalScrollIndicator = false
-fooScrollView.indicatorStyle = .Black
-fooScrollView.delaysContentTouches = false
-fooScrollView.canCancelContentTouches = false
-fooScrollView.minimumZoomScale = 5
-fooScrollView.maximumZoomScale = 3
-fooScrollView.bouncesZoom = false
-fooScrollView.keyboardDismissMode = .OnDrag
+let defaultTableView = UITableView()
